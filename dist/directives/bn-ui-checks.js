959523ae583f0a872876945e658b71a7ae843dc7
Bndy <zb@bndy.net>
2017-01-18 20:40:02 +0800

    add(bn-ui-checks): new directive & demo

---
+++ dist/directives/bn-ui-checks.js
@@ -0,0 +1,49 @@
+/*!
+ * angular-more v2.0.0 (https://github.com/BndyNet/angular-more#readme)
+ * (c) 2014-2017 Bndy.Net (http://www.bndy.net)
+ */
+
+"use strict";
+angular.module("bn.ui.checks", ["ngSanitize"]).directive("bnUiChecks", function() {
+  return {
+    restrict: "E",
+    replace: true,
+    scope: {
+      model: "=ngModel",
+      label: "@",
+      source: "=",
+      multiple: "=",
+      withIcon: "="
+    },
+    template: '<div class="bn-ui-checks form-group">\n    <label ng-bind-html="label" ng-if="label"></label>\n    <div>\n        <div class="btn-group">\n            <label class="btn btn-default" ng-repeat="(key, value) in source" \n                ng-class="{active: isExisted(value)}" ng-click="select(value)">\n\n                <i class="glyphicon fa fa-fw {{multiple?\'glyphicon-check fa-check-square-o\':\'glyphicon-ok fa-dot-circle-o\'}}" ng-show="isExisted(value) && withIcon"></i>\n                <i class="glyphicon fa fa-fw {{multiple?\'glyphicon-unchecked fa-square-o\':\'fa-circle-o\'}}" ng-show="!isExisted(value) && withIcon"></i>\n                <span ng-bind="key"></span>\n            </label>\n        </div>\n    </div>\n</div>',
+    link: function(scope, ele, attrs) {
+      if (!scope.model && scope.multiple) {
+        scope.model = [];
+      }
+      if (!scope.withIcon) {
+        scope.withIcon = false;
+      }
+      scope.isExisted = function(value) {
+        if (typeof scope.model === "undefined") {
+          return false;
+        }
+        if (scope.multiple) {
+          return scope.model.indexOf(value) >= 0;
+        } else {
+          return value.toString() === scope.model.toString();
+        }
+      };
+      return scope.select = function(value) {
+        if (scope.multiple) {
+          if (scope.model.indexOf(value) < 0) {
+            return scope.model.push(value);
+          } else {
+            return scope.model.splice(scope.model.indexOf(value), 1);
+          }
+        } else {
+          return scope.model = value;
+        }
+      };
+    }
+  };
+});
---
+++ src/directives/bn-ui-checks.coffee
@@ -0,0 +1,43 @@
+﻿"use strict"
+
+angular.module "bn.ui.checks", ["ngSanitize"]
+    .directive "bnUiChecks", 
+    ->
+        restrict: "E"
+        replace: true
+        scope:
+            model: "=ngModel"
+            label: "@"
+            source: "="
+            multiple: "="
+            withIcon: "="
+        template: '''
+            <div class="bn-ui-checks form-group">
+                <label ng-bind-html="label" ng-if="label"></label>
+                <div>
+                    <div class="btn-group">
+                        <label class="btn btn-default" ng-repeat="(key, value) in source" 
+                            ng-class="{active: isExisted(value)}" ng-click="select(value)">
+
+                            <i class="glyphicon fa fa-fw {{multiple?\'glyphicon-check fa-check-square-o\':\'glyphicon-ok fa-dot-circle-o\'}}" ng-show="isExisted(value) && withIcon"></i>
+                            <i class="glyphicon fa fa-fw {{multiple?\'glyphicon-unchecked fa-square-o\':\'fa-circle-o\'}}" ng-show="!isExisted(value) && withIcon"></i>
+                            <span ng-bind="key"></span>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        '''
+        link: (scope, ele, attrs) ->
+            scope.model = [] if not scope.model and scope.multiple
+            scope.withIcon = false if not scope.withIcon
+            scope.isExisted = (value) ->
+                return false if typeof scope.model is "undefined"
+                if scope.multiple then scope.model.indexOf(value) >= 0 else value.toString() == scope.model.toString()
+            scope.select = (value) ->
+                if scope.multiple
+                    if scope.model.indexOf(value) < 0 
+                        scope.model.push(value)
+                    else
+                        scope.model.splice scope.model.indexOf(value), 1
+                else 
+                    scope.model = value
